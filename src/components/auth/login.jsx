e32a36fff24870e7a3febe0d8b3ee59e7cb47172
youssef tadlaoui <usftadlaoui@gmail.com>
2023-10-26 22:24:57 +0100

    add_axios_login-succss

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,33 +1,54 @@
-import React from "react"
+import axios from "axios"
+import React, { useState } from "react"
 
 export const Login=()=>{
+   const [formData,setData]=useState({
+    email : "",
+    password : ""
+  })
+  const handelInputChange=(e)=>{
+    console.log(e.target.value)
+        setData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+  }
+
+const handelSubmuit= async (e) => {
+    e.preventDefault();
+    try{
+      const response= await axios.post("http://localhost:8000/api/auth/login",formData);
+      console.log(response.data); 
+    }catch(err){
+            console.log(err.message);
+    }
+}
 return (
-  <div class="relative flex flex-col items-center justify-center h-screen overflow-hidden">
-  <div class="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
-      <h1 class="text-3xl font-semibold text-center text-gray-700">DaisyUI</h1>
-      <form class="space-y-4">
+  <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+  <div className="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
+      <h1 className="text-3xl font-semibold text-center text-gray-700">Allo livreur</h1>
+      <form className="space-y-4" onSubmit={handelSubmuit} method="POST">
           <div>
-              <label class="label">
-                  <span class="text-base label-text">Email</span>
+              <label className="label" >
+                  <span className="text-base label-text">Email</span>
               </label>
-              <input type="text" placeholder="Email Address" class="w-full input input-bordered" />
+              <input name="email" type="text" placeholder="Email Address" className="w-full input input-bordered" onChange={handelInputChange} />
           </div>
           <div>
-              <label class="label">
-                  <span class="text-base label-text">Password</span>
+              <label className="label">
+                  <span className="text-base label-text">Password</span>
               </label>
-              <input type="password" placeholder="Enter Password"
-                  class="w-full input input-bordered" />
+              <input name="password"  type="password" placeholder="Enter Password"
+                  className="w-full input input-bordered" onChange={handelInputChange}/>
           </div>
           <div className="flex justify-between">
-          <a href="#" class="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
+          <a href="/forgetPassword" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
           <div>
           </div>
-          <a href="/register" class="text-xs text-gray-600 hover:underline hover:text-blue-600">Créer nouveau compte</a>
-          </div>
-          
+          <a href="/register" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Créer nouveau compte</a>
+          </div>       
           <div>
-              <button class="btn btn-block">Login</button>
+              <button type="submit" className="btn btn-block">Login</button>
           </div>
       </form>
   </div>
