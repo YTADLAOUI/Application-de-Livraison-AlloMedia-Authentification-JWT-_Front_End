88ab383cd26107524e7d9332b9cd48326ab4e1c7
youssef tadlaoui <usftadlaoui@gmail.com>
2023-10-26 14:38:35 +0100

    add_route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {Login} from './components/auth/login';
 import { Register } from './components/auth/register';
 import './index.css'
 function App() {
   return (
-    //  <Login/>
-    <Register/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 export default App;
