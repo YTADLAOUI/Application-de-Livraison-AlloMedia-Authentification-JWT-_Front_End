0ad937eb865421cd83019caa2449339d7562773e
youssef tadlaoui <usftadlaoui@gmail.com>
2023-10-29 21:04:28 +0100

    notification_etat

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 export const Register= ()=>{
 
@@ -11,9 +13,8 @@ export const Register= ()=>{
     image:"",
     role:""
 });
-
 const handleInputChange = (e) => {
-  console.log(e.target.value)
+  // console.log(e.target.value)
   setData({
       ...formData,
       [e.target.name]: e.target.value
@@ -25,16 +26,23 @@ const handleSubmit = async (e) => {
       console.log(formData);
       // Faites la requête POST vers votre API d'enregistrement
       const response = await axios.post("http://localhost:8000/api/auth/register", formData);
-      console.log(response.data); 
+      console.log(response.data);
+          if(response.data.remplie) toast.warning(response.data.remplie, { position: toast.POSITION.TOP_RIGHT});
+          if(response.data.utilisateurExiste) toast.warning(response.data.utilisateurExiste, { position: toast.POSITION.TOP_RIGHT});
+          if(response.data.check) toast.success(response.data.check, { position: toast.POSITION.TOP_RIGHT});
+
   } catch (error) {
       console.log(error.message);
       console.log(error);
+      toast.error('try agine your registration !', {
+        position: toast.POSITION.TOP_RIGHT
+    });
   }
 };
 
   return (
-    <div className="relative flex flex-col justify-center h-screen overflow-hidden">
-    <div className="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-xl">
+    <div className="relative flex flex-col justify-center h-screen overflow-hidden p-6">
+    <div className=" max-w-sm w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-xl">
         <h1 className="text-3xl font-semibold text-center text-gray-700">Allo Livreur</h1>
        
           <form className="space-y-4" onSubmit={handleSubmit}>
@@ -82,9 +90,9 @@ const handleSubmit = async (e) => {
             <div>
               <button type="submit" className="btn btn-block">Register</button>
             </div>
-            {/* <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> */}
           </form>
         </div>
+        <ToastContainer />
       </div>
   )
 }
