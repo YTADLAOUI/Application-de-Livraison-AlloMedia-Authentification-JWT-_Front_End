9d72e75cdc0d00d91ec2f3c66a3c083e0e29a7fa
youssef tadlaoui <usftadlaoui@gmail.com>
2023-10-31 14:08:41 +0100

    switch_to_useForm

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import Cookies from "js-cookie"
 import React, { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
 import { useSelector,useDispatch } from 'react-redux'
 import { useNavigate } from "react-router-dom"
 import {toast } from 'react-toastify';
@@ -13,29 +14,29 @@ export const Login=()=>{
             if(data.payload?.error) toast.error(data.payload.error, { position: toast.POSITION.TOP_RIGHT});
         },[data]
     )
-    
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate=useNavigate();
-   const [formData,setData]=useState({
-    email : "",
-    password : ""
-  })
+//    const [formData,setData]=useState({
+//     email : "",
+//     password : ""
+//   })
 
-  const handelInputChange=(e)=>{
-    // console.log(e.target.value)
-        setData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-  }
+//   const handelInputChange=(e)=>{
+//     // console.log(e.target.value)
+//         setData({
+//             ...formData,
+//             [e.target.name]: e.target.value
+//         });
+//   }
 
-const handelSubmuit= async (e) => {
-    e.preventDefault();
+const handelSubmuit= async (data) => {
+    // e.preventDefault();
     try{
-      const response= await axios.post("http://localhost:8000/api/auth/login",formData);
+      const response= await axios.post("http://localhost:8000/api/auth/login",data);
     //   console.log(JSON.stringify(response.data.user))
-        Cookies.set('user',JSON.stringify(response.data.user));
-        Cookies.set('token',response.data.loginToken);
-        navigate("/dashboard");
+    Cookies.set('user',JSON.stringify(await response.data.user));
+    Cookies.set('token',await response.data.loginToken);
+    navigate("/dashboard");
     }catch(err){
             console.log(err.message);
     }
@@ -44,19 +45,21 @@ return (
   <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden p-6">
   <div className=" max-w-sm w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
       <h1 className="text-3xl font-semibold text-center text-gray-700">Allo marhaba</h1>
-      <form className="space-y-4" onSubmit={handelSubmuit} method="POST">
+      <form className="space-y-4" onSubmit={handleSubmit(handelSubmuit)} method="POST">
           <div>
               <label className="label" >
                   <span className="text-base label-text">Email</span>
               </label>
-              <input name="email" type="text" placeholder="Email Address" className="w-full input input-bordered" onChange={handelInputChange} />
+              <input name="email"  {...register("email", { required: "Email is required" })} type="text" placeholder="Email Address" className="w-full input input-bordered" />
+              <span className="text-red-500 text-sm">{errors.email?.message}</span>
           </div>
           <div>
               <label className="label">
                   <span className="text-base label-text">Password</span>
               </label>
               <input name="password"  type="password" placeholder="Enter Password"
-                  className="w-full input input-bordered" onChange={handelInputChange}/>
+                  className="w-full input input-bordered"{...register("password", { required: "Password is required" })}/>
+                   <span className="text-red-500 text-sm">{errors.password?.message}</span>
           </div>
           <div className="flex justify-between">
           <a href="/forgetPassword" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
